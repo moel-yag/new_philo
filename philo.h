36efd752711e2f0ed99892f4bc403af91419b2eb
medel <moel-yag@student.1337.ma>
2025-07-06 18:29:03 +0100

    Update philo.h

--- philo.h
+++ philo.h
@@ -13,50 +13,49 @@
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <errno.h>
-# include <limits.h>
 # include <pthread.h>
 # include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <string.h>
 # include <sys/time.h>
 # include <unistd.h>
+# include <limits.h>
 
 typedef struct s_philo
 {
-    pthread_mutex_t *left_fork;
-    pthread_mutex_t *right_fork;
-    pthread_mutex_t *stop_mutex;
-    pthread_mutex_t *print_mutex;
-    pthread_mutex_t meal_mutex;
-    pthread_t       threads;
-    int             id;
-    int             meals_eaten;
-    long long       last_meal;
-    long long       start_time;
-    int             *stop;
-    int             time_to_eat;
-    int             time_to_sleep;
-    int             time_to_die;
-}                   t_philo;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	*stop_mutex;
+	pthread_mutex_t	*print_mutex;
+	pthread_mutex_t	meal_mutex;
+	pthread_t		threads;
+	int				id;
+	int				meals_eaten;
+	long long		last_meal;
+	long long		start_time;
+	int				*stop;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				time_to_die;
+}					t_philo;
 
 typedef struct s_sim
 {
-    t_philo         *philos;
-    int             num_philo;
-    int             time_to_die;
-    int             meal_target;
-    int             *stop;
-    pthread_mutex_t *stop_mutex;
-    pthread_mutex_t *print_mutex;
-}   t_sim;
+	t_philo			*philos;
+	int				num_philo;
+	int				time_to_die;
+	int				meal_target;
+	int				*stop;
+	pthread_mutex_t	*stop_mutex;
+	pthread_mutex_t	*print_mutex;
+}					t_sim;
 
-long long   get_time(void);
-void        *routine(void *arg);
-bool        is_valid_arg(int ac, char *args[]);
-long long   ft_atoi(const char *str);
-void        *monitor(void *arg);
-void        print_status(t_philo *philo, const char *status);
+long long			get_time(void);
+void				*routine(void *arg);
+bool				is_valid_arg(int ac, char *args[]);
+long long			ft_atoi(const char *str);
+void				*monitor(void *arg);
+void				print_status(t_philo *philo, const char *status);
+void				precise_usleep(long duration, t_philo *philo);
 
 #endif
