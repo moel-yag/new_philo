3c1aded6a6ce3fd375c0576c832d6a3027053ef4
medel <moel-yag@student.1337.ma>
2025-07-06 18:29:57 +0100

    Update philo.c

--- philo.c
+++ philo.c
@@ -12,89 +12,108 @@
 
 #include "philo.h"
 
-int main(int ac, char **av)
+int	init_mutexes(pthread_mutex_t *forks, pthread_mutex_t *stop_mutex,
+	pthread_mutex_t *print_mutex, int num_philo)
 {
-    t_philo         philo[200];
-    pthread_mutex_t forks[200];
-    pthread_mutex_t stop_mutex;
-    pthread_mutex_t print_mutex;
-    int             stop;
-    int             num_philo;
-    int             i;
-    long long       simulation_start;
-    t_sim           sim;
-    pthread_t       monitor_thread;
+	int	i;
 
-    stop = 0;
-    if (ac != 5 && ac != 6)
-    {
-        printf("Usage: %s <number_of_philo> <time_to_die> <time_to_eat> \
-<time_to_sleep> [number_of_meals]\n", av[0]);
-        return (1);
-    }
-    if (!is_valid_arg(ac, av))
-    {
-        printf("Error: Invalid arguments\n");
-        return (2);
-    }
-    num_philo = ft_atoi(av[1]);
-    if (num_philo <= 0 || num_philo > 200)
-    {
-        printf("Error: Philosophers must be between 1-200\n");
-        return (2);
-    }
-    
-    // Initialize mutexes
-    pthread_mutex_init(&stop_mutex, NULL);
-    pthread_mutex_init(&print_mutex, NULL);
-    for (i = 0; i < num_philo; i++)
-        pthread_mutex_init(&forks[i], NULL);
+	if (pthread_mutex_init(stop_mutex, NULL))
+		return (1);
+	if (pthread_mutex_init(print_mutex, NULL))
+		return (1);
+	i = -1;
+	while (++i < num_philo)
+		if (pthread_mutex_init(&forks[i], NULL))
+			return (1);
+	return (0);
+}
 
-    // Initialize philosophers
-    simulation_start = get_time();
-    for (i = 0; i < num_philo; i++)
-    {
-        philo[i].id = i + 1;
-        philo[i].meals_eaten = 0;
-        philo[i].last_meal = simulation_start;
-        philo[i].left_fork = &forks[i];
-        philo[i].right_fork = &forks[(i + 1) % num_philo];
-        philo[i].stop = &stop;
-        philo[i].stop_mutex = &stop_mutex;
-        philo[i].print_mutex = &print_mutex;
-        philo[i].start_time = simulation_start;
-        philo[i].time_to_eat = ft_atoi(av[3]);
-        philo[i].time_to_sleep = ft_atoi(av[4]);
-        philo[i].time_to_die = ft_atoi(av[2]);
-        pthread_mutex_init(&philo[i].meal_mutex, NULL);
-    }
+void	init_philos(t_philo *philos, pthread_mutex_t *forks,
+	t_sim *sim, char **av)
+{
+	int			i;
+	long long	start;
+	int			num_philo;
 
-    // Start philosopher threads
-    for (i = 0; i < num_philo; i++)
-        pthread_create(&philo[i].threads, NULL, routine, &philo[i]);
+	num_philo = ft_atoi(av[1]);
+	start = get_time();
+	i = -1;
+	while (++i < num_philo)
+	{
+		philos[i].id = i + 1;
+		philos[i].meals_eaten = 0;
+		philos[i].last_meal = start;
+		philos[i].left_fork = &forks[i];
+		philos[i].right_fork = &forks[(i + 1) % num_philo];
+		philos[i].stop = sim->stop;
+		philos[i].stop_mutex = sim->stop_mutex;
+		philos[i].print_mutex = sim->print_mutex;
+		philos[i].start_time = start;
+		philos[i].time_to_eat = ft_atoi(av[3]);
+		philos[i].time_to_sleep = ft_atoi(av[4]);
+		philos[i].time_to_die = ft_atoi(av[2]);
+		pthread_mutex_init(&philos[i].meal_mutex, NULL);
+	}
+}
 
-    // Set up and start monitor thread
-    sim.philos = philo;
-    sim.num_philo = num_philo;
-    sim.time_to_die = ft_atoi(av[2]);
-    sim.stop = &stop;
-    sim.stop_mutex = &stop_mutex;
-    sim.print_mutex = &print_mutex;
-    sim.meal_target = (ac == 6) ? ft_atoi(av[5]) : -1;
-    pthread_create(&monitor_thread, NULL, monitor, &sim);
+int	start_threads(t_philo *philos, pthread_t *monitor_thread,
+	t_sim *sim, int num_philo)
+{
+	int	i;
 
-    // Wait for threads to finish
-    for (i = 0; i < num_philo; i++)
-        pthread_join(philo[i].threads, NULL);
-    pthread_join(monitor_thread, NULL);
+	i = -1;
+	while (++i < num_philo)
+		if (pthread_create(&philos[i].threads, NULL, routine, &philos[i]))
+			return (1);
+	if (pthread_create(monitor_thread, NULL, monitor, sim))
+		return (1);
+	return (0);
+}
 
-    // Cleanup resources
-    for (i = 0; i < num_philo; i++)
-    {
-        pthread_mutex_destroy(&forks[i]);
-        pthread_mutex_destroy(&philo[i].meal_mutex);
-    }
-    pthread_mutex_destroy(&stop_mutex);
-    pthread_mutex_destroy(&print_mutex);
-    return (0);
+void	cleanup(t_philo *philos, pthread_mutex_t *forks,
+	pthread_mutex_t *stop_mutex, pthread_mutex_t *print_mutex, int num_philo)
+{
+	int	i;
+
+	i = -1;
+	while (++i < num_philo)
+	{
+		pthread_mutex_destroy(&forks[i]);
+		pthread_mutex_destroy(&philos[i].meal_mutex);
+	}
+	pthread_mutex_destroy(stop_mutex);
+	pthread_mutex_destroy(print_mutex);
+}
+
+int	main(int ac, char **av)
+{
+	t_philo			philos[200];
+	pthread_mutex_t	forks[200];
+	pthread_mutex_t	stop_mutex;
+	pthread_mutex_t	print_mutex;
+	t_sim			sim;
+	pthread_t		monitor_thread;
+	int				stop;
+	int				num_philo;
+
+	stop = 0;
+	if (ac < 5 || ac > 6)
+		return (printf("Usage: %s number t_die t_eat t_sleep [meals]\n", av[0]), 1);
+	if (!is_valid_arg(ac, av))
+		return (printf("Error: Invalid arguments\n"), 2);
+	num_philo = ft_atoi(av[1]);
+	if (num_philo <= 0 || num_philo > 200)
+		return (printf("Error: Philosophers 1-200\n"), 2);
+	if (init_mutexes(forks, &stop_mutex, &print_mutex, num_philo))
+		return (printf("Error: Mutex init failed\n"), 3);
+	sim = (t_sim){philos, num_philo, ft_atoi(av[2]), -1, &stop,
+		&stop_mutex, &print_mutex};
+	if (ac == 6)
+		sim.meal_target = ft_atoi(av[5]);
+	init_philos(philos, forks, &sim, av);
+	if (start_threads(philos, &monitor_thread, &sim, num_philo))
+		return (printf("Error: Thread creation failed\n"), 4);
+	pthread_join(monitor_thread, NULL);
+	cleanup(philos, forks, &stop_mutex, &print_mutex, num_philo);
+	return (0);
 }
