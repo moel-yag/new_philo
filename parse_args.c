3dd6a0fb96afe1887f3a198b83f6475f22ff3d89
medel <moel-yag@student.1337.ma>
2025-07-06 18:29:30 +0100

    Update parse_args.c

--- parse_args.c
+++ parse_args.c
@@ -12,70 +12,70 @@
 
 #include "philo.h"
 
-int ft_strlen(const char *str)
+int	ft_strlen(const char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-long long ft_atoi(const char *str)
+long long	ft_atoi(const char *str)
 {
-    long long   result;
-    int         i;
+	long long	result;
+	int			i;
 
-    result = 0;
-    i = 0;
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        result = result * 10 + (str[i] - '0');
-        if (result > INT_MAX)
-            return (-1);
-        i++;
-    }
-    if (str[i] != '\0')
-        return (-1);
-    return (result);
+	result = 0;
+	i = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		if (result > INT_MAX)
+			return (-1);
+		i++;
+	}
+	if (str[i] != '\0')
+		return (-1);
+	return (result);
 }
 
-bool    ft_isdigit(char c)
+bool	ft_isdigit(char c)
 {
-    return (c >= '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
 
-bool    large_number(const char *arg, int i)
+bool	large_number(const char *arg, int i)
 {
-    long long   num;
+	long long	num;
 
-    num = ft_atoi(arg);
-    if (num < 0 || (i == 1 && num == 0) || (i == 0 && num > 200))
-        return (true);
-    return (false);
+	num = ft_atoi(arg);
+	if (num < 0 || (i == 1 && num == 0) || (i == 0 && num > 200))
+		return (true);
+	return (false);
 }
 
-bool    is_valid_arg(int ac, char *args[])
+bool	is_valid_arg(int ac, char *args[])
 {
-    int j;
-    int i;
+	int	j;
+	int	i;
 
-    i = 1;
-    while (i < ac)
-    {
-        if (!args[i] || !*args[i])
-            return (false);
-        if (large_number(args[i], i - 1))
-            return (false);
-        j = 0;
-        while (j < (int)ft_strlen(args[i]))
-        {
-            if (!ft_isdigit(args[i][j]))
-                return (false);
-            j++;
-        }
-        i++;
-    }
-    return (true);
+	i = 1;
+	while (i < ac)
+	{
+		if (!args[i] || !*args[i])
+			return (false);
+		if (large_number(args[i], i - 1))
+			return (false);
+		j = 0;
+		while (j < (int)ft_strlen(args[i]))
+		{
+			if (!ft_isdigit(args[i][j]))
+				return (false);
+			j++;
+		}
+		i++;
+	}
+	return (true);
 }
